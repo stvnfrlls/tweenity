aa92efd8068b5699e2a8b5707977778c361ea4bd
stvnfrlls <stvnfrlls@gmail.com>
2022-10-15 00:27:33 +0800

    Added student number generator

--- admin/addStudent.php
+++ admin/addStudent.php
@@ -1,3 +1,13 @@
+<?php 
+    require_once("../utils/server.php");
+    $get_userID = ("SELECT COUNT(userID) FROM studentrecord");
+    $result = $mysqli->query($get_userID);
+    $count = $result->fetch_assoc();
+    $studentCounter = $count["COUNT(userID)"] + 1;
+    $year = date("Y");
+    $studentNumber = $year."-".$studentCounter."-SP";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,6 +21,53 @@
 <body>
     <h1>Add Student</h1>
     <a href="../index.php">Home</a>
+
+    <form action="<?php $_SERVER[" PHP_SELF"] ?>" method="post">
+        <div class="container">
+            <h1>STUDENT INFORMATION</h1>
+            <div class="row">
+                <label for="SR_number">Student Number</label>
+                <input type="text" name="SR_number" id="SR_number" value="<?php echo $studentNumber ?>" readonly>
+            </div>
+            <div class="row">
+                <label for="SR_fname">First Name</label>
+                <input type="text" name="SR_fname" id="SR_fname">
+                <label for="SR_mname">Middle Name</label>
+                <input type="text" name="SR_mname" id="SR_mname">
+                <label for="SR_lname">Last Name</label>
+                <input type="text" name="SR_lname" id="SR_lname">
+            </div>
+            <div class="row">
+                <label for="SR_age">Age</label>
+                <input type="number" name="SR_age" id="SR_age">
+                <label for="SR_birthday">Birthday</label>
+                <input type="date" name="SR_birthday" id="SR_birthday">
+            </div>
+            <div class="row">
+                <label for="SR_address">Address</label>
+                <input type="text" name="SR_address" id="SR_address">
+                <label for="SR_guardian">Guardian</label>
+                <input type="text" name="SR_guardian" id="SR_guardian">
+                <label for="SR_contact">Contact</label>
+                <input type="text" name="SR_contact" id="SR_contact">
+            </div>
+
+            <h1>SCHOOL INFORMATION</h1>
+            <div class="row">
+                <label for="SR_grade">Year Level</label>
+                <input type="text" name="SR_grade" id="SR_grade">
+                <label for="SR_section">Section</label>
+                <input type="text" name="SR_section" id="SR_section">
+            </div>
+            <div class="row">
+                <label for="SR_age">Age</label>
+                <input type="number" name="SR_age" id="SR_age">
+                <label for="SR_birthday">Birthday</label>
+                <input type="date" name="SR_birthday" id="SR_birthday">
+            </div>
+        </div>
+        <input type="submit" value="Register Student">
+    </form>
 </body>
 
 </html>
