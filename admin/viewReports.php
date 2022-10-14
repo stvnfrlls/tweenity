194e0cfecedb14885c6f97f3ac810f64b1d433bd
stvnfrlls <stvnfrlls@gmail.com>
2022-10-14 15:48:47 +0800

    Added
    -add student
    -edit student
    -edit grades
    -view reports

---
+++ admin/viewReports.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View Reports</title>
+</head>
+
+<body>
+    <h1>View Reports</h1>
+    <a href="../index.php">Home</a>
+</body>
+
+</html>
