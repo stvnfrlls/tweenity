a86e876025ce61b5e4765c572a467e44dd5888c6
stvnfrlls <stvnfrlls@gmail.com>
2022-10-14 16:42:17 +0800

    Added admin dashboard

---
+++ admin/dashboard.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Tools</title>
+</head>
+
+<body>
+    <h1>Admin Dashboard</h1>
+    <nav>
+        <ul>
+            <li><a href="../index.php">Home</a></li>
+            <li><a href="admin/addStudent.php">Add Student</a></li>
+            <li><a href="admin/editStudent.php">Edit Student</a></li>
+            <li><a href="admin/editGrades.php">Edit Grades</a></li>
+            <li><a href="admin/viewReports.php">View Reports</a></li>
+        </ul>
+    </nav>
+</body>
+
+</html>
